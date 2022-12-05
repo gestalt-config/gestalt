cdc938578d8abd03e0839c169c067e794be73fca
Colin Redmond <34700833+credmond-git@users.noreply.github.com>
2022-12-04 19:53:01 -0800

    scan a path for Kubernetes secrets. Addresses issue 30 (#70)
    scan a path for kubernetes secrets. Addresses issue https://github.com/gestalt-config/gestalt/issues/30

---
+++ gestalt-core/src/main/java/org/github/gestalt/config/source/KubernetesSecretConfigSource.java
@@ -0,0 +1,173 @@
+package org.github.gestalt.config.source;
+
+import org.github.gestalt.config.exceptions.GestaltException;
+import org.github.gestalt.config.tag.Tags;
+import org.github.gestalt.config.utils.Pair;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Load a config source as kubernetes files secrets.
+ * https://kubernetes.io/docs/concepts/configuration/secret/#projection-of-secret-keys-to-specific-paths
+ *
+ * @author Colin Redmond
+ */
+public class KubernetesSecretConfigSource implements ConfigSource {
+    public static final String K8_SECRET_CONFIG = "k8Secret";
+
+    private final Path path;
+    private final UUID id = UUID.randomUUID();
+    private final Tags tags;
+
+    /**
+     * Constructor for a Kubernetes Files Config Source.
+     *
+     * @param file where to load the directory with the configuration
+     * @throws GestaltException any exceptions.
+     */
+    public KubernetesSecretConfigSource(File file) throws GestaltException {
+        this(Objects.requireNonNull(file, "Kubernetes Secret file can not be null").toPath(), Tags.of());
+    }
+
+    /**
+     * Constructor for a Kubernetes Files Config Source.
+     *
+     * @param file where to load the directory with the configuration
+     * @param tags tags associated with the source
+     * @throws GestaltException any exceptions.
+     */
+    public KubernetesSecretConfigSource(File file, Tags tags) throws GestaltException {
+        this(Objects.requireNonNull(file, "Kubernetes Secret file can not be null").toPath(), tags);
+    }
+
+    /**
+     * Constructor for a Kubernetes Files Config Source.
+     *
+     * @param path where to load the directory with the configuration
+     * @throws GestaltException any exceptions.
+     */
+    public KubernetesSecretConfigSource(Path path) throws GestaltException {
+        this(path, Tags.of());
+    }
+
+    /**
+     * Constructor for a Kubernetes Files Config Source.
+     *
+     * @param path where to load the directory with the configuration
+     * @param tags tags associated with the source
+     * @throws GestaltException any exceptions.
+     */
+    public KubernetesSecretConfigSource(Path path, Tags tags) throws GestaltException {
+        this.path = validatePath(path);
+        this.tags = tags;
+    }
+
+    private Path validatePath(Path path) throws GestaltException {
+        Objects.requireNonNull(path, "Kubernetes Secret path can not be null");
+        if (!Files.isDirectory(path)) {
+            throw new GestaltException("Kubernetes Secret path does not exist at: " + path);
+        } else if (isEmpty(path)) {
+            throw new GestaltException("Kubernetes Secret path is empty: " + path);
+        }
+        return path;
+    }
+
+    private boolean isEmpty(Path path) throws GestaltException {
+        try (DirectoryStream<Path> directory = Files.newDirectoryStream(path)) {
+            return !directory.iterator().hasNext();
+        } catch (IOException e) {
+            throw new GestaltException("IOException while testing if path is empty.", e);
+        }
+    }
+
+    /**
+     * The path to the file.
+     *
+     * @return path to the file
+     */
+    public Path getPath() {
+        return path;
+    }
+
+    @Override
+    public boolean hasStream() {
+        return false;
+    }
+
+    @Override
+    public InputStream loadStream() throws GestaltException {
+        throw new GestaltException("Unsupported operation loadStream on an KubernetesFilesConfigSource");
+    }
+
+    @Override
+    public boolean hasList() {
+        return true;
+    }
+
+    @Override
+    public List<Pair<String, String>> loadList() throws GestaltException {
+        List<Pair<String, String>> results = new ArrayList<>();
+        if (Files.isDirectory(path)) {
+            try (DirectoryStream<Path> directory = Files.newDirectoryStream(path)) {
+                for (Path secretFile : directory) {
+                    if (Files.isRegularFile(secretFile)) {
+                        String key = secretFile.getFileName().toString();
+                        String value = Files.readString(secretFile);
+                        Pair<String, String> secret = new Pair<>(key, value);
+                        results.add(secret);
+                    }
+                }
+            } catch (IOException e) {
+                throw new GestaltException("IOException while loading kubernetes secrets from path: " + path);
+            }
+        }
+        return results;
+    }
+
+    @Override
+    public String format() {
+        return K8_SECRET_CONFIG;
+    }
+
+    @Override
+    public String name() {
+        return "Kubernetes Secret source: " + path.toString();
+    }
+
+    @Override
+    public UUID id() {  //NOPMD
+        return id;
+    }
+
+    @Override
+    public Tags getTags() {
+        return tags;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof KubernetesSecretConfigSource)) {
+            return false;
+        }
+        KubernetesSecretConfigSource that = (KubernetesSecretConfigSource) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
---
+++ gestalt-core/src/test/java/org/github/gestalt/config/source/KubernetesSecretConfigSourceTest.java
@@ -0,0 +1,153 @@
+package org.github.gestalt.config.source;
+
+import org.github.gestalt.config.exceptions.GestaltException;
+import org.github.gestalt.config.tag.Tags;
+import org.github.gestalt.config.utils.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class KubernetesSecretConfigSourceTest {
+
+    private Path kubernetesPath;
+
+    @BeforeAll
+    public void setup() throws URISyntaxException {
+        URL testFileURL = FileConfigSourceTest.class.getClassLoader().getResource("test.properties");
+        Path testFileDir = Paths.get(testFileURL.toURI());
+        kubernetesPath = testFileDir.getParent().resolve("kubernetes");
+    }
+
+    @Test
+    void getPath() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        Assertions.assertEquals(kubernetesPath, source.getPath());
+    }
+
+    @Test
+    void setupNullPath() {
+        NullPointerException exception = Assertions.assertThrows(NullPointerException.class,
+            () -> new KubernetesSecretConfigSource((Path) null));
+        Assertions.assertEquals("Kubernetes Secret path can not be null", exception.getMessage());
+    }
+
+    @Test
+    void setupNullFile() {
+        NullPointerException exception = Assertions.assertThrows(NullPointerException.class,
+            () -> new KubernetesSecretConfigSource((File) null));
+        Assertions.assertEquals("Kubernetes Secret file can not be null", exception.getMessage());
+    }
+
+    @Test
+    void setupNotADirectory() throws URISyntaxException {
+        URL testFileURL = FileConfigSourceTest.class.getClassLoader().getResource("test.properties");
+        Path testFileDir = Paths.get(testFileURL.toURI());
+
+        GestaltException exception = Assertions.assertThrows(GestaltException.class,
+            () -> new KubernetesSecretConfigSource(testFileDir.toFile()));
+        Assertions.assertEquals("Kubernetes Secret path does not exist at: " + testFileDir, exception.getMessage());
+    }
+
+    @Test
+    void setupEmptyDirectory() throws IOException {
+        Path tempDir = Files.createTempDirectory("test" + Math.random());
+
+        GestaltException exception = Assertions.assertThrows(GestaltException.class, () -> new KubernetesSecretConfigSource(tempDir));
+        Assertions.assertEquals("Kubernetes Secret path is empty: " + tempDir.toString(), exception.getMessage());
+    }
+
+    @Test
+    void hasStream() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        Assertions.assertFalse(source.hasStream());
+    }
+
+    @Test
+    void loadStream() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+
+        Assertions.assertThrows(GestaltException.class, source::loadStream);
+    }
+
+    @Test
+    void hasList() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        Assertions.assertTrue(source.hasList());
+    }
+
+    @Test
+    void loadList() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        var results = source.loadList();
+        assertThat(results).hasSize(3)
+                           .contains(new Pair<>("db.host.password", "abcdef"))
+                           .contains(new Pair<>("db.host.uri", "jdbc:postgresql://localhost:5432/mydb1"))
+                           .contains(new Pair<>("subservice.booking.token", "111222333"));
+    }
+
+    @Test
+    void format() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        Assertions.assertEquals(KubernetesSecretConfigSource.K8_SECRET_CONFIG, source.format());
+    }
+
+    @Test
+    void name() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        assertThat(source.name()).startsWith("Kubernetes Secret source: ");
+    }
+
+    @Test
+    void idTest() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        KubernetesSecretConfigSource source2 = new KubernetesSecretConfigSource(kubernetesPath);
+        Assertions.assertNotEquals(source.id(), source2.id());
+    }
+
+    @Test
+    void getTags() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        Assertions.assertEquals(Tags.of(), source.getTags());
+    }
+
+    @Test
+    void getTagsWithTags() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath, Tags.of("env", "dev"));
+        Assertions.assertEquals(Tags.of("env", "dev"), source.getTags());
+    }
+
+    @Test
+    void getTagsWithTagsFile() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath.toFile(), Tags.of("env", "dev"));
+        Assertions.assertEquals(Tags.of("env", "dev"), source.getTags());
+    }
+
+    @Test
+    void testEquals() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        KubernetesSecretConfigSource source2 = new KubernetesSecretConfigSource(kubernetesPath);
+
+        Assertions.assertEquals(source, source);
+        Assertions.assertNotEquals(source, source2);
+        Assertions.assertNotEquals(source, null);
+        Assertions.assertNotEquals(source, 1L);
+    }
+
+    @Test
+    void testHashCode() throws GestaltException {
+        KubernetesSecretConfigSource source = new KubernetesSecretConfigSource(kubernetesPath);
+        Assertions.assertTrue(source.hashCode() != 0);
+    }
+}
