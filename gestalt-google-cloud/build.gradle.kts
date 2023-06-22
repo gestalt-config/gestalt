1fa7e89127564a2da639481c9625123f83a37704
Colin Redmond <colin.redmond@outlook.com>
2023-06-21 21:24:42 -0700

    update dependencies.

--- gestalt-google-cloud/build.gradle.kts
+++ gestalt-google-cloud/build.gradle.kts
@@ -1,16 +1,14 @@
 plugins {
-    id("gestalt.java-library-conventions")
-    id("gestalt.java-test-conventions")
-    id("gestalt.java-code-quality-conventions")
-    id("gestalt.java-publish-conventions")
+  id("gestalt.java-library-conventions")
+  id("gestalt.java-test-conventions")
+  id("gestalt.java-code-quality-conventions")
+  id("gestalt.java-publish-conventions")
 }
 
 dependencies {
-    implementation(project(":gestalt-core"))
-    implementation(platform("com.google.cloud:libraries-bom:" + libs.versions.gcpLibraryBOM.get()))
-    implementation("com.google.cloud:google-cloud-storage")
-    implementation("com.google.cloud:google-cloud-secretmanager")
-
+  implementation(project(":gestalt-core"))
+  implementation(libs.google.storage)
+  implementation(libs.google.secret)
 }
 
 tasks.jar {
