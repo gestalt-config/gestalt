9681169ca09195bdf17cf03f52afde33e63bae1a
Colin Redmond <colin.redmond@outlook.com>
2024-02-22 18:50:23 -0800

    feat: part 1 of config container

---
+++ gestalt-core/src/main/java/org/github/gestalt/config/decoder/ConfigDecoder.java
@@ -0,0 +1,43 @@
+package org.github.gestalt.config.decoder;
+
+import org.github.gestalt.config.entity.Config;
+import org.github.gestalt.config.entity.ConfigContainer;
+import org.github.gestalt.config.node.ConfigNode;
+import org.github.gestalt.config.reflect.TypeCapture;
+import org.github.gestalt.config.tag.Tags;
+import org.github.gestalt.config.utils.GResultOf;
+
+import java.util.Optional;
+
+/**
+ * Decodes a generic optional type.
+ *
+ * @author <a href="mailto:colin.redmond@outlook.com"> Colin Redmond </a> (c) 2024.
+ */
+public final class ConfigDecoder implements Decoder<ConfigContainer<?>> {
+
+    @Override
+    public Priority priority() {
+        return Priority.MEDIUM;
+    }
+
+    @Override
+    public String name() {
+        return "Config";
+    }
+
+    @Override
+    public boolean canDecode(String path, Tags tags, ConfigNode node, TypeCapture<?> type) {
+        return Optional.class.isAssignableFrom(type.getRawType());
+    }
+
+    @Override
+    public GResultOf<ConfigContainer<?>> decode(String path, Tags tags, ConfigNode node, TypeCapture<?> type, DecoderContext decoderContext) {
+        // decode the generic type of the optional. Then we will wrap the result into an Optional
+        GResultOf<?> configValue = decoderContext.getDecoderService()
+            .decodeNode(path, tags, node, type.getFirstParameterType(), decoderContext);
+
+            return configValue.mapWithError((it) -> new ConfigContainer(path, tags, decoderContext, it.results(), type));
+        }
+    }
+}
