6790ce02b80aa890685b3bf87a9cf51aaa8d60ad
Colin Redmond <colin.redmond@outlook.com>
2024-03-06 07:00:09 -0800

    fix: correct static import PMD error.

--- gestalt-core/src/test/java/org/github/gestalt/config/decoder/DecoderContextTest.java
+++ gestalt-core/src/test/java/org/github/gestalt/config/decoder/DecoderContextTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DecoderContextTest {
 
     @Test
